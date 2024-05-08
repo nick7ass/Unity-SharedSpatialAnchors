20a9187a65308901173e70029fde48cec2248445
Nicklas <145897592+nick7ass@users.noreply.github.com>
2024-05-08 10:22:21 +0200

    new globe asset

---
+++ Assets/WorldPoliticalMapGlobeEdition/Scripts/Core/Input/IInputProxy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace WPM {
+
+    public interface IInputProxy {
+
+        bool GetButtonDown(string buttonName);
+        bool GetButtonUp(string buttonName);
+        bool GetMouseButton(int buttonIndex);
+        bool GetMouseButtonDown(int buttonIndex);
+        bool GetMouseButtonUp(int buttonIndex);
+        Vector3 mousePosition { get; }
+        bool touchSupported { get; }
+        int touchCount { get; }
+        bool GetKey(string name);
+        bool GetKey(KeyCode keyCode);
+        bool GetKeyDown(KeyCode keyCode);
+        Touch GetTouch(int index);
+        float GetAxis(string axisName);
+        Touch[] touches { get; }
+        LocationService location { get; }
+    }
+
+}
